fd1677f43087c3b0a53f600b322468085c2ba327
erso <edusillva784@gmail.com>
2024-07-31 12:15:00 -0300

    Added message display and dataframe saving to main

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-from solution.solution import SelicCalc
-from solution.calculator_handler import CalculatorHandler
-from datetime import datetime
 import json
+import pandas as pd
+from datetime import datetime
+from solution.calculator_handler import CalculatorHandler
+
+def display_message(df:pd.DataFrame):
+    print(df.loc[df['Amount earned'].idxmax()])
+    print(df[df.index == datetime(2016,10,28)])
+
+def save_dataframe(df:pd.DataFrame, file_name):
+    df.to_csv(file_name)
 
 if __name__ == '__main__':
     environment_variables = json.load(open('config.json','r'))
-    c = CalculatorHandler()
-    #calc = SelicCalc()
-
-    #calc.calc_amount_earned(
-    #    start_date=date(2010, 1, 1),
-    #    end_date=date(2021, 3, 1),
-    #    capital=657.43,
-    #    frequency="daily",
-    #    save_csv=False,
-    #)
-
-
-# fazer a leitura do json 
-# minha ideia eh fzr com que ele receba um dicionario com parametros e use a estrategia de dicionarios de funcoes para setar os atributos
-# ja tem isso pronto no sgs_handler, então tudo certo
-# percebi que o sgs handler deveria ser responsável por setar essas variaveis, dado que recebeu um dicionario
-# depois desse ajuste, salvar o dataframe do intervalo total e fazer testes
-# provavelmente deve ter algum erro, os valores estão bem altos.
-# cogitei fazer um database_handler só de brincadeira que iria salvar o .csv. 
+    c = CalculatorHandler(environment_variables)
+    c.calculate()
+    display_message(c.dataframe)
+    save_dataframe(c.dataframe,'solution.csv')
