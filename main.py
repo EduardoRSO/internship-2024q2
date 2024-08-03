25ba2e9d9cd1b8fb8e2713668d067a3654010491
erso <edusillva784@gmail.com>
2024-08-02 23:35:23 -0300

    Fixed display_message

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import json
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
 from solution.calculator_handler import CalculatorHandler
 
-def display_message(df:pd.DataFrame):
-    print(df.loc[df['Amount earned'].idxmax()])
-    print(df[df.index == datetime(2016,10,28)])
+def display_message(df: pd.DataFrame, range_of: int = 500):
+    df = df.reset_index()
+    max_amount_id = df['acc_var'].idxmax()
+    row_end = df.iloc[max_amount_id]
+    row_start = df.iloc[max_amount_id-500]
+ 
+    print(
+        f"\nThe best day to invest is {row_start['Date']}, with an amount earned of {row_end['Amount earned']} after {range_of} days "
+        f"({row_start['Date']} to {row_end['Date']})"
+    )
 
 def save_dataframe(df:pd.DataFrame, file_name):
     df.to_csv(file_name)
@@ -16,3 +23,6 @@ if __name__ == '__main__':
     c.calculate()
     display_message(c.dataframe)
     save_dataframe(c.dataframe,'solution.csv')
+    save_dataframe(c.dataframe,'df_raw.csv')
+
+
