f94facccc40efd800f31d527ba498f0062881cab
erso <edusillva784@gmail.com>
2024-08-04 16:04:34 -0300

    Fixed display_message time range

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import json
 import pandas as pd
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from solution.calculator_handler import CalculatorHandler
 
 def display_message(df: pd.DataFrame, range_of: int = 500):
     df = df.reset_index()
     max_amount_id = df['acc_var'].idxmax()
     row_end = df.iloc[max_amount_id]
-    row_start = df.iloc[max_amount_id-500]
- 
+    row_start = df[df.Date >= row_end.Date-timedelta(range_of)].head(1)
+    
     print(
-        f"\nThe best day to invest is {row_start['Date']}, with an amount earned of {row_end['Amount earned']} after {range_of} days "
-        f"({row_start['Date']} to {row_end['Date']})"
+        f"\nThe best day to invest is {row_start['Date'].iloc[0]}, with an amount earned of {row_end['acc_var']} after {range_of} days "
+        f"({row_start['Date'].iloc[0]} to {row_end['Date']})"
     )
 
 def save_dataframe(df:pd.DataFrame, file_name):
